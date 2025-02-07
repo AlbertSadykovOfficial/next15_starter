ebc0f0fc618b23d08f80bda0b335460d9b134b14
ALBERT SADYKOV <albertsadykov@ro.ru>
2025-02-07 18:27:47 +0300

    Button props

--- app/src/components/UI/Button.tsx
+++ app/src/components/UI/Button.tsx
@@ -3,6 +3,7 @@ import type { TChildren } from "@/models/Types/TChildren";
 export default function Button(props: TChildren) {
   return (
     <button
+      {...props}
       className={
         "button text-white bg-orange-500 shadow-orange-500/50 hover:shadow-orange-500/40 middle center rounded-lg py-3 px-6 font-sans text-xs font-bold shadow-md transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
       }
